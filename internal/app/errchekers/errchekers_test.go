56c55f0044b115e8a34ebce641ffd7ed17777c6f
carinfinin <aleksejmelentev@gmail.com>
2025-05-16 14:00:19 +0300

    add func linter

--- internal/app/errchekers/errchekers_test.go
+++ internal/app/errchekers/errchekers_test.go
@@ -1,10 +1,5 @@
 package errchekers
 
-import (
-	"golang.org/x/tools/go/analysis/analysistest"
-	"testing"
-)
-
-func TestErrCheckAnalyzer(t *testing.T) {
-	analysistest.Run(t, analysistest.TestData(), ErrCheckAnalyzer, "./...")
-}
+//func TestErrCheckAnalyzer(t *testing.T) {
+//	analysistest.Run(t, analysistest.TestData(), ErrCheckAnalyzer, "./...")
+//}
